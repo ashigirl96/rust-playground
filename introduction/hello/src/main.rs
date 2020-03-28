4e5510f08c26221db888aa4a2abadc50cbb5845c
ashigirl96 <reon@mma.cs.tsukuba.ac.jp>
2020-03-29 01:10:22 +0900

    update README

--- introduction/hello/src/main.rs
+++ introduction/hello/src/main.rs
@@ -1,9 +1,13 @@
 fn add(x: f64, y: f64) -> f64 {
-   x + y
+    x + y
 }
 
 fn main() {
     println!("Hello, world!");
     println!("2.5 + 4.2 = {0}", add(2.5, 4.2));
     println!("PI is {0:.2}", std::f64::consts::PI);
+    let mut x = [1, 2, 3];
+    println!("{0:?}, {1:?}", x, &x);
+    x.swap(0, 2);
+    println!("{0:?}, {1:?}", x, &x);
 }
