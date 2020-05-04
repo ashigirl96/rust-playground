6a0a57b618b1543ea18419f0f7787f64d08dc4fa
ashigirl96 <reon@mma.cs.tsukuba.ac.jp>
2020-05-05 03:14:36 +0900

    cast types

--- user_defined/examples/cast_type.rs
+++ user_defined/examples/cast_type.rs
@@ -1,1 +1,22 @@
-fn main() {}
+fn f1(x: &[usize]) -> usize {
+    x.len()
+}
+
+fn f2(x: &mut [usize]) {
+    let len = f1(x);
+    x[0] = len;
+}
+
+fn main() {
+    let v4: Vec<u8> = From::from("hello");
+    println!("{:?}", v4);
+
+    let f1 = 5.53e+3_f32;
+    println!("{}", f1);
+    let i2: i32 = unsafe { std::mem::transmute(f1) };
+    println!("{}", i2);
+
+    let mut x = vec![0; 3];
+    f2(&mut x);
+    println!("{:?}", x);
+}
